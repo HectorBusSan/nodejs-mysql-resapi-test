e5d9afa48fff8446f9379be2619b569e95a6b761
HectorBussan <hector141599@hotmail.com>
2022-12-20 09:03:54 -0600

    pong

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,5 +1,6 @@
 import {pool} from "../db.js"
 export const ping=async(req,res)=>{
-    const [result]= await pool.query("Select 1 + 1 as result");
-    res.json(result[0].result);
+    res.json({
+        message:"pong"
+    })
 }
